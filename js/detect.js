3c635d343234441684495416a5ae9b78dc3c9d09
Quyet Nguyen <nvquyet2224@gmail.com>
2018-05-08 17:27:48 +0700

    initiate

---
+++ js/detect.js
@@ -0,0 +1,54 @@
+var ua = navigator.userAgent;
+var match = ua.match('MSIE (.)');
+var versions = match && match.length > 1 ? match[1] : 'unknown';
+var isTouchDevice =  "ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch  || (navigator.msMaxTouchPoints>0) || (navigator.maxTouchPoints > 0);
+var IEMobile = ua.match(/IEMobile/i);
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isIE9 = /MSIE 9/i.test(ua); 
+var isIE10 = /MSIE 10/i.test(ua);
+var isIE11 = /rv:11.0/i.test(ua) && !IEMobile  ? true : false;
+var isIE = false || !!document.documentMode;
+var isEdge = !isIE && !!window.StyleMedia && !isIE11;
+
+var ios, android, blackBerry, UCBrowser, Operamini, firefox, windows, smartphone, tablet,touchscreen, all;
+var isMobile = {
+  ios: (function(){
+    return ua.match(/iPhone|iPad|iPod/i);
+  }()),
+  android: (function(){
+    return ua.match(/Android/i);
+  }()),
+  blackBerry: (function(){
+    return ua.match(/BB10|Tablet|Mobile/i);
+  }()),
+  UCBrowser: (function(){
+    return ua.match(/UCBrowser/i);
+  }()),
+  Operamini: (function(){
+    return ua.match(/Opera Mini/i);
+  }()),
+  
+  windows: (function(){
+    return ua.match(/IEMobile/i);
+  }()),
+  smartphone: (function(){
+	return (ua.match(/Android|BlackBerry|Tablet|Mobile|iPhone|iPad|iPod|Opera Mini|IEMobile/i) && window.innerWidth <= 440 && window.innerHeight <= 740);
+  }()),
+  tablet: (function(){
+    return (ua.match(/Android|BlackBerry|Tablet|Mobile|iPhone|iPad|iPod|Opera Mini|IEMobile/i) && window.innerWidth <= 1366 && window.innerHeight <= 800);
+  }()),
+  all: (function(){
+    return ua.match(/Android|BlackBerry|Tablet|Mobile|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
+  }())
+};
+
+if(isTouchDevice  && isMobile.all !== null){
+	var TouchLenght = true;
+}else if(isMobile.tablet && isFirefox || isMobile.smartphone && isFirefox ){
+	var TouchLenght = true;
+}else{
+	var TouchLenght = false;
+}
+
+if(isIE9 || isIE10 || isIE11){ $('html,body').addClass('isIE')}
+if(isIE9 || isIE10 || isIE11){ $('html,body').addClass('fixJumpy')}
