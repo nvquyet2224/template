c23b12685f9abffdb8b77d5067c2fd979b483ec4
Quyet Nguyen <nvquyet2224@gmail.com>
2018-09-10 10:53:13 +0700

    Update slider7

--- slider/js/main7.js
+++ slider/js/main7.js
@@ -1,85 +1,165 @@
-var app = new PIXI.Application(800, 600, { antialias: true });
+
+
+var xWidth = 1920;
+var yHeight = 1080;
+
+var app = new PIXI.Application(xWidth, yHeight);
 document.body.appendChild(app.view);
 
 app.stage.interactive = true;
-
-var bg = PIXI.Sprite.fromImage('img/BGrotate.jpg');
-
-bg.anchor.set(0.5);
-
-bg.x = app.renderer.width / 2;
-bg.y = app.renderer.height / 2;
-
-app.stage.addChild(bg);
-
 var container = new PIXI.Container();
-container.x = app.renderer.width / 2;
-container.y = app.renderer.height / 2;
-
-// add a bunch of sprites
-var bgFront = PIXI.Sprite.fromImage('img/SceneRotate.jpg');
-bgFront.anchor.set(0.5);
-
-//var light2 = PIXI.Sprite.fromImage('img/LightRotate2.png');
-//light2.anchor.set(0.5);
-
-//var light1 = PIXI.Sprite.fromImage('img/LightRotate1.png');
-//light1.anchor.set(0.5);
-
-var panda =  PIXI.Sprite.fromImage('img/panda.png');
-panda.anchor.set(0.5);
-
-//container.addChild(bgFront, light2, light1, panda);
-container.addChild(bgFront, panda);
-
 app.stage.addChild(container);
 
-// let's create a moving shape
-var thing = new PIXI.Graphics();
-//thing.lineStyle(0);
-thing.beginFill(0xFFFF0B, 1);
-thing.drawCircle(400, 300,150);
-thing.endFill();
-app.stage.addChild(thing);
-container.mask = thing;
+var bg = PIXI.Sprite.fromImage('img/1.jpg');
+bg.width = app.renderer.width;
+bg.height = app.renderer.height;
 
-//app.stage.addChild(thing);
-//thing.x = app.renderer.width / 2;
-//thing.y = app.renderer.height / 2;
-//thing.lineStyle(0);
-//container.mask = thing;
+//bg.alpha = 0.3;
+
+container.addChild(bg);
+
+
+var displacementSprite = PIXI.Sprite.fromImage('img/clouds.jpg');
+var displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+app.stage.addChild(displacementSprite);
+
+container.filters = [displacementFilter];
+
+
+app.stage.on('mousemove', onPointerMove);
+
+function onPointerMove(eventData) {
+	
+	//ring.visible = true;
+	//var x = eventData.data.global.x - 100;
+	//var y = eventData.data.global.y - displacementSprite.height /2;
+	
+    //displacementSprite.x = eventData.data.global.x - 100;
+    //displacementSprite.y = eventData.data.global.y - displacementSprite.height /2;
+
+    //ring.x = eventData.data.global.x - 25;
+    //ring.y = eventData.data.global.y;
+	
+	TweenMax.to(displacementSprite, 1, {x: eventData.data.global.x, y: eventData.data.global.y});
+	
+}
+
+
+app.ticker.add(function(timer) {
+	//console.log(timer);
+	//displacementSprite.x += options.autoPlaySpeed[0] * delta;
+	//displacementSprite.y += options.autoPlaySpeed[1];
+	//displacementSprite.scale.set(timer);
+	//TweenMax.to(displacementSprite.scale, 1, {x: 200 * timer, y: 70 * timer});
+});
+
+
+/*
+var padding = 100;
+var bounds = new PIXI.Rectangle(
+    -padding,
+    -padding, 
+    app.renderer.width + padding * 2, 
+    app.renderer.height + padding * 2
+);
+var maggots = [];
+
+for (var i = 0; i < 20; i++)
+{
+    var maggot =  PIXI.Sprite.fromImage('img/maggot.png');
+    maggot.anchor.set(0.5);
+    container.addChild(maggot);
+
+    maggot.direction = Math.random() * Math.PI * 2;
+    maggot.speed = 1;
+    maggot.turnSpeed = Math.random() - 0.8;
+
+    maggot.x = Math.random() * bounds.width;
+    maggot.y = Math.random() * bounds.height;
+
+    maggot.scale.set(1 + Math.random() * 0.3);
+    maggot.original = new PIXI.Point();
+	maggot.original.copy(maggot.scale);
+    maggots.push(maggot);
+
+}
+
+var displacementSprite = PIXI.Sprite.fromImage('img/displace.png');
+var displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+app.stage.addChild(displacementSprite);
+
+container.filters = [displacementFilter];
+
+displacementFilter.scale.x = 110;
+displacementFilter.scale.y = 110;
+
+//var ring = PIXI.Sprite.fromImage('img/ring.png');
+
+//ring.anchor.set(0.5);
+
+//ring.visible = false;
+
+//app.stage.addChild(ring);
+
+var bg = PIXI.Sprite.fromImage('img/bkg-grass.jpg');
+bg.width = app.renderer.width;
+bg.height = app.renderer.height;
+
+bg.alpha = 0.3;
+
+container.addChild(bg);
+
+app.stage
+    .on('mousemove', onPointerMove)
+    .on('touchmove', onPointerMove);
+
+function onPointerMove(eventData)
+{
+    //ring.visible = true;
+	var x = eventData.data.global.x - 100;
+	var y = eventData.data.global.y - displacementSprite.height /2;
+	
+    //displacementSprite.x = eventData.data.global.x - 100;
+    //displacementSprite.y = eventData.data.global.y - displacementSprite.height /2;
+
+    //ring.x = eventData.data.global.x - 25;
+    //ring.y = eventData.data.global.y;
+	
+	
+}
 
 var count = 0;
 
-app.stage.on('pointertap', function() {
-    if (!container.mask) {
-        //container.mask = thing;
-    }
-    else {
-        //container.mask = null;
-    }
-});
-
-var help = new PIXI.Text('Click or tap to turn masking on / off.', { 
-    fontFamily: 'Arial',
-    fontSize: 12, 
-    fontWeight:'bold', 
-    fill: 'white'
-});
-help.y = app.renderer.height - 26;
-help.x = 10;
-app.stage.addChild(help);
-
 app.ticker.add(function() {
-    //bg.rotation += 0.01;
-    //bgFront.rotation -= 0.01;
-    //light1.rotation += 0.02;
-    //light2.rotation += 0.01;
-	
-    panda.scale.x = 1 + Math.sin(count) * 0.04;
-    panda.scale.y = 1 + Math.cos(count) * 0.04;
+    
+    count += 0.05;
 
-	thing.scale.set(1 + Math.sin(count) * 0.01);
-	count += 0.1;
-	
+    for (var i = 0; i < maggots.length; i++) {
+        var maggot = maggots[i];
+
+        maggot.direction += maggot.turnSpeed * 0.01;
+        maggot.x += Math.sin(maggot.direction) * maggot.speed;
+        maggot.y += Math.cos(maggot.direction) * maggot.speed;
+
+        maggot.rotation = -maggot.direction - Math.PI/2;
+        maggot.scale.x = maggot.original.x + Math.sin(count) * 0.2;
+
+        // wrap the maggots around as the crawl
+        if (maggot.x < bounds.x) {
+            maggot.x += bounds.width;
+        }
+        else if (maggot.x > bounds.x + bounds.width) {
+            maggot.x -= bounds.width;
+        }
+
+        if (maggot.y < bounds.y) {
+            maggot.y += bounds.height;
+        }
+        else if (maggot.y > bounds.y + bounds.height) {
+            maggot.y -= bounds.height;
+        }
+    }
 });
+*/
