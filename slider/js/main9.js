41ab121befed5b1b330b453e78cbc6e0b769b69a
Quyet Nguyen <nvquyet2224@gmail.com>
2018-09-12 14:24:42 +0700

    Update scale

--- slider/js/main9.js
+++ slider/js/main9.js
@@ -1,100 +1,119 @@
-    
-var autoPlay = true;
-var wacky = false;
-var fullScreen = true;
-var autoPlaySpeed = [3,3];
-var displaceScale  = [200,70];
-var centerSprites = false;
-var autoFix = false;
-var xWidth = window.innerWidth;
-var yHeight = window.innerHeight;
+var renderer, 
+	stage,
+	autoPlay, 
+	fullScreen,
+	autoFix,
+	displacementSprite, 
+	displacementFilter, 
+	mouseX = 0, 
+	mouseY = 0,
+	xWidth  = window.innerWidth,
+	yHeight = window.innerHeight;
 
-var renderer, stage, container, screen, displacementSprite, displacementFilter, graphics, mask, maskRadius = 100;
-var rafID, mouseX, mouseY;
+renderer = new PIXI.autoDetectRenderer( xWidth, yHeight, {transparent: true});
+stage = new PIXI.Container();
+document.body.appendChild(renderer.view);
+//.add('map', 'img/dmaps/2048x2048/clouds.jpg');	
+var banner;
+var loader = PIXI.loader;
+loader.add('banner', 'img/banner.jpg')
+	  .add('map', 'img/dmaps/2048x2048/clouds.jpg');
 
-//Sóng ra vào
-function init_02() {
-	renderer = new PIXI.autoDetectRenderer( xWidth, yHeight, {transparent: true});
-	stage = new PIXI.Container();
-	
-	var src = 'img/test04.jpg';
-	displacementSprite  = new PIXI.Sprite.fromImage(src);
-	displacementFilter  = new PIXI.filters.DisplacementFilter(displacementSprite);
-	pixi_02(src);
-	wave_02();
-}
-
-function pixi_02(src){
-	// Add canvas to the HTML
-	document.body.appendChild( renderer.view );
-
-	// Enable Interactions
+loader.load(function(loader, resources){
 	stage.interactive = true;
+	banner = new PIXI.Sprite(loader.resources.banner.texture);
+	displacementSprite = new PIXI.Sprite(loader.resources.map.texture);
+	displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
 	
-	// Fit renderer to the screen
-	if (fullScreen === true ) {
-	  renderer.view.style.objectFit = 'cover';
-	  renderer.view.style.width     = '100%';
-	  renderer.view.style.height    = '100%';
-	  renderer.view.style.top       = '50%';
-	  renderer.view.style.left      = '50%';
-	  renderer.view.style.webkitTransform = 'translate( -50%, -50% ) scale(1.2)';           
-	  renderer.view.style.transform = 'translate( -50%, -50% ) scale(1.2)';           
-	} else {
-	  renderer.view.style.maxWidth  = '100%';
-	  renderer.view.style.top       = '50%';
-	  renderer.view.style.left      = '50%';
-	  renderer.view.style.webkitTransform = 'translate( -50%, -50% )';           
-	  renderer.view.style.transform = 'translate( -50%, -50% )';          
-	}
-
-	displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-
-	// Set the filter to stage and set some default values for the animation
-	stage.filters = [displacementFilter];        
-
-	displacementSprite.scale.x = 2;
-	displacementSprite.scale.y = 2;
-
-	// PIXI tries to fit the filter bounding box to the renderer so we optionally bypass
-	displacementFilter.autoFit = autoFix;
+	//Apply elements to stage
+	stage.addChild(banner);
 	
-	stage.addChild( displacementSprite );
-
-	var bg = new PIXI.Sprite.fromImage(src);
-	stage.addChild( bg );
-}
-
-
-//Wave automatic
-function wave_02() {
-	var ticker = new PIXI.ticker.Ticker();
-	ticker.autoStart = autoPlay;
-	var y = 0;
-	var plus = true;
-	var waveH = Math.round(yHeight/2);
-	ticker.add(function(delta) {
-		y++;
-		if(y >= 50) {
-			y = 50;
-		}
-		displacementSprite.y = y;
-		renderer.render(stage);
-	});
+	//Set the filter to stage
+	//displacementSprite.scale.x = 1.2;
+	//displacementSprite.scale.y = 1.2;
+	
+	//displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+	//stage.addChild(displacementSprite);
+	
+	//displacementFilter.scale.x = 3;
+	//displacementFilter.scale.y = 3;
 		
+	stage.filters = [displacementFilter]; 
+	
+	//Draw canvas when loaded all images
+	//renderer.render(stage);
+	render();
+	
+	//renderer.render(stage);
+	
+});
+
+var update = false;
+var delta = 1;
+var count = 0;
+var baseTimeline = new TimelineMax();
+/*		
+function render(){
+	
+	if(update) {
+		
+		//displacementFilter.scale.x = 10 * Math.sin(delta*1.2);
+		displacementFilter.scale.y = 10 * Math.sin(delta*1.2);
+		delta += 0.05;
+		count += 1;
+		if(count == 200) {
+			update = false;
+			count = 0;
+		}
+		requestAnimationFrame(render); 
+	}
+	renderer.render(stage);
+}
+*/
+
+var baseTimeline = new TimelineMax();
+ 
+function render(){
+	
+	//console.log(Math.sin(0));
+	//requestAnimationFrame(render);
+	//baseTimeline.clear();
+	//baseTimeline.to(displacementFilter.scale, 0.5, { x: 200 , y: 100 * Math.sin(5), repeat:-1, yoyo:true, onComplete: function(){
+		//console.log('aa');	
+	//}});
+	
+	//baseTimeline.to(displacementFilter.scale, 1, { x: 200, y: 70, onComplete: function() {
+		//console.log('asasa');
+	//}});
+	if(update) {
+		//requestAnimationFrame(render);
+		displacementFilter.scale.x = 0;
+		displacementFilter.scale.y += 10 * Math.sin(mouseY);
+	}
+	
+	//TweenMax.to(displacementFilter.scale, 1, { x: 0 , y: 5 * Math.sin(mouseY), repeat:-1, yoyo:true});
+	renderer.render(stage);
+	
 }
 
-init_02();
+
 
 window.addEventListener("mousewheel", MouseWheelHandler, false);
-window.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-	
 function MouseWheelHandler(event) {
-	if(event.deltaY > 0) {
-		console.log('down');	
-	}else {
-		console.log('up');	
-	}
+	
+	//update = true;
+	//mouseX = 30;
+	//mouseY = 10;
+	//console.log(event);
+	//mouseY
+	//update = true;
+	//count = 0;
+	//TweenMax.clear();
+	update = true;
+	mouseY = 100;
+	TweenMax.to(displacementFilter.scale, 1, { x: 0 , y: mouseY, yoyo:true, repeat:1, ease:Linear.easeNone, onUpdate: render});
+
 }
+
 
 
