a8af007f39975886a7372208195faf9c6440472b
Quyet Nguyen <nvquyet2224@gmail.com>
2018-10-10 09:53:13 +0700

    speed test aa

--- js/detect.js
+++ js/detect.js
@@ -1,9 +1,1 @@
-var ua = navigator.userAgent;
-var isFirefox = typeof InstallTrigger !== 'undefined';
-var isEdge = /Edge/i.test(ua);
-var isIE = /MSIE 9|MSIE 10|rv:11.0/i.test(ua);
-var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
-
-if(isIE){
-	//$('body').addClass('isIE');
-}
+var ua=navigator.userAgent,isFirefox="undefined"!=typeof InstallTrigger,isEdge=/Edge/i.test(ua),isIE=/MSIE 9|MSIE 10|rv:11.0/i.test(ua),isMobile=/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-
-(function($) {
-
-	//Show data
-	setTimeout(function(){
-
-		$('.loadicon').fadeOut(300, function(){
-			$('.wrap').stop().animate({'opacity':1}, 500 ,'linear', function () {
-				//onScroll();
-				//Start();
-			});
-		});
-		
-	},1000);
-
-}(jQuery));
-
-
+!function(n){setTimeout(function(){n(".loadicon").fadeOut(300,function(){n(".wrap").stop().animate({opacity:1},500,"linear",function(){})})},1e3)}(jQuery);
