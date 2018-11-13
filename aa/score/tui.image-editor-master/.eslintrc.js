97078654ad02ebebe616301203ff6520294550a3
Quyet Nguyen <nvquyet2224@gmail.com>
2018-11-13 18:17:52 +0700

    update data farbic

---
+++ aa/score/tui.image-editor-master/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "extends": "tui/es6",
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true,
+        "jasmine": true,
+        "jquery": true,
+        "es6": true
+    },
+    "globals": {
+        "fabric": true,
+        "tui": true,
+        "loadFixtures": true
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    'rules': {
+        'prefer-destructuring': ['error', {
+            VariableDeclarator: {array: true, object: true},
+            AssignmentExpression: {array: false, object: false}
+        }]
+    }
+};
