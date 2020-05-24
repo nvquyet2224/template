3e1b4df6913479567a826fb456b660019d6343cf
nvquyet2224 <nvquyet2224@gmail.com>
2020-05-25 01:07:29 +0700

    -Update tool
    -Implement mask

--- tools/js/main.js
+++ tools/js/main.js
@@ -14,7 +14,7 @@ function processFiles(files) {
 }
 
 var count = 0;
-	var mash;
+var mash;
 function uploadFile(file) {
 	//var type = file.type.split('/')[1];
 	var reader = new FileReader();
@@ -22,115 +22,121 @@ function uploadFile(file) {
 	reader.onload = function (e) {
 
 		var data = e.target.result;
-		fabric.Image.fromURL(data, function (img) {
+
+		fabric.Image.fromURL('images/phone/iphone_upload.png', function (imgMask) {
+
 			var ctxR = canvas.height / canvas.width,
-				imgR = img.height / img.width,
+				imgR = imgMask.height / imgMask.width,
 				imgH = imgR * canvas.width,
-				xScale = (imgR > ctxR && img.width < canvas.width) ? 1 : canvas.width / img.width;
-			yScale = (imgR > ctxR && img.width < canvas.width) ? 1 : imgH / img.height;
+				xScale = (imgR > ctxR && imgMask.width < canvas.width) ? 1 : canvas.width / imgMask.width;
+			yScale = (imgR > ctxR && imgMask.width < canvas.width) ? 1 : imgH / imgMask.height;
 
-			var obj = img.set({
-				scaleX: xScale,
-				scaleY: yScale
+			maskObj = imgMask.set({
+				scaleX: xScale / 2,
+				scaleY: yScale / 2,
+				selectable: false
 			});
-			
-			if(count == 0) {
-				obj.selectable = false;
-			}
-			if(count == 1) {
-				//obj.selectable = false;
-				//canvas.setOverlayImage('images/phone/iphone-6s-plus.png');
-			}
 
-			canvas.centerObject(obj);
-			if(count == 1) {
-				canvas.add(obj).setOverlayImage(obj);
-			}
-			else {
+			fabric.Image.fromURL(data, function (img) {
+
+				var ctxR = canvas.height / canvas.width,
+					imgR = img.height / img.width,
+					imgH = imgR * canvas.width,
+					xScale = (imgR > ctxR && img.width < canvas.width) ? 1 : canvas.width / img.width;
+				yScale = (imgR > ctxR && img.width < canvas.width) ? 1 : imgH / img.height;
+
+				var obj = img.set({
+					scaleX: xScale / 4,
+					scaleY: yScale / 4,
+					globalCompositeOperation: 'source-atop'
+				});
+
+				canvas.centerObject(maskObj);
+				canvas.centerObject(obj);
+
+				canvas.add(maskObj);
 				canvas.add(obj);
-			}
-			
-			count++;
+			});
+
 		});
+
 
 	}
 
 	reader.readAsDataURL(file);
 }
 
+function init() {
+
+	fabric.Image.fromURL('images/phone/iphone-6s-plus_full.png', function (img) {
+		var ctxR = canvas.height / canvas.width,
+			imgR = img.height / img.width,
+			imgH = imgR * canvas.width,
+			xScale = (imgR > ctxR && img.width < canvas.width) ? 1 : canvas.width / img.width;
+		yScale = (imgR > ctxR && img.width < canvas.width) ? 1 : imgH / img.height;
+
+		var obj = img.set({
+			scaleX: xScale / 2,
+			scaleY: yScale / 2,
+			selectable: false
+		});
+
+		canvas.centerObject(obj);
+		canvas.add(obj);
+	});
+
+	fabric.Image.fromURL('images/phone/iphone-6s-plus.png', function (img) {
+		var ctxR = canvas.height / canvas.width,
+			imgR = img.height / img.width,
+			imgH = imgR * canvas.width,
+			xScale = (imgR > ctxR && img.width < canvas.width) ? 1 : canvas.width / img.width;
+		yScale = (imgR > ctxR && img.width < canvas.width) ? 1 : imgH / img.height;
+
+		var obj = img.set({
+			scaleX: xScale / 2,
+			scaleY: yScale / 2,
+			selectable: false
+		});
+
+		canvas.centerObject(obj);
+		canvas.add(obj).setOverlayImage(obj);
+
+	});
+
+
+}
+
 var down = false;
 // Page Ready
 (function () {
-	canvas = new fabric.Canvas('canvas', { preserveObjectStacking: true});
+	canvas = new fabric.Canvas('canvas', { preserveObjectStacking: true });
+
 	//canvas.setOverlayImage('images/phone/iphone-6s-plus.png');
 	canvas.on({
 		'object:added': function (event) {
-			// console.log(count);
-			//if(count == 1) {
-			//	canvas.moveTo(canvas.item(0),10);
-			//}
-			//canvas.renderAll();
 		},
 		'object:modified': function (event) {
-			// if(count == 1) {
-			// 	canvas.moveTo(canvas.item(0),2);
-			// }
 		},
 		'object:selected': function (event) {
-			// if(canvas.item(1)) {
-			// 	canvas.moveTo(canvas.item(1),3);
-			// 	//canvas.renderAll();
-			// }
 		},
 		'selection:updated': function (event) {
-			// if(count == 1) {
-			// 	canvas.moveTo(canvas.item(0),2);
-			// }
 		},
 		'selection:cleared': function (event) {
 			console.log('asa');
 		},
 		'mouse:down': function (event) {
-			//console.log('down');
-			// if(count == 2) {
-			// 	canvas.moveTo(canvas.item(0),2);
-			// }
 			down = true;
 		},
 		'mouse:up': function (event) {
-			//console.log('up');
-			// if(count == 1) {
-			// 	canvas.moveTo(canvas.item(0),2);
-			// }
 			down = false;
 		},
 		'mouse:move': function (event) {
-			//console.log('move');
-			
-			// if(count == 2 && down) {
-			// 	canvas.moveTo(canvas.item(0),2);
-			// 	canvas.renderAll();
-			// }
-			// if(canvas.item(1)) {
-			// 	canvas.moveTo(canvas.item(0),10);
-			// }
-			// canvas.renderAll();
 		}
 	});
-	
+
+	init();
+
 	fabric.util.requestAnimFrame(function render() {
-		// if(canvas.item(0)) {
-		// 	//canvas.moveTo(canvas.item(0),2);
-		// 	//canvas.renderAll();
-		// }
-		// if(count == 2 && down) {
-		// 	canvas.moveTo(mash,10);
-		// 	console.log('update');
-		// 	//canvas.renderAll();
-		// }
-		//var objcount = canvas.getObjects().length;
-		//console.log(objcount);
-		//canvas.moveTo(mash,objcount++);
 		canvas.renderAll();
 		fabric.util.requestAnimFrame(render);
 	});
@@ -139,7 +145,7 @@ var down = false;
 		var files = byThis.target.files;
 		processFiles(files);
 	}
-	$('#addText').click(function(){
+	$('#addText').click(function () {
 		//canvas.moveTo(mash,2);
 		//canvas.renderAll();
 	});
