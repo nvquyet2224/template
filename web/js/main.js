f9c0cb589770d67044b512fe3749915e1dcf4126
Quyet Nguyen <nvquyet2224@gmail.com>
2020-08-18 09:16:20 +0700

    -done live4

--- web/js/main.js
+++ web/js/main.js
@@ -4,230 +4,185 @@ var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.
 	ua
 );
 
-var docEl,
-	pageWrap,
-	scrollTop,
-	isOverlay = false;
-
-// Lock touch body
-function lockBody() {
-	if (window.pageYOffset) {
-		scrollTop = window.pageYOffset;
-		pageWrap.css({ top: - (scrollTop) });
-	}
-	isOverlay = true;
-	docEl.addClass('fs-no-scroll');
-}
-
-// Unlock touch body
-function unlockBody() {
-	isOverlay = false;
-	docEl.removeClass('fs-no-scroll');
-	pageWrap.css({ top: 'auto' });
-	window.scrollTo(0, scrollTop);
-	window.setTimeout(function () {
-		scrollTop = null;
-	}, 0);
-}
-
 function inputHolder() {
 	$('.fs-group input[type="text"], .fs-group input[type="password"]').focus(function (e) {
 		$(this).parent().parent().removeClass('fs-show-error');
 	});
 }
 
-// Events Common
-function fsEvent() {
 
-	// Open menu
-	$('.fs-nav-but').on('click', function (e) {
-		if ($(this).hasClass('active')) {
-			unlockBody();
-			$('.fs-nav-but, .fs-navigation').removeClass('active');
-		} else {
-			lockBody();
-			$('.fs-nav-but, .fs-navigation').addClass('active');
-		}
+var gamePlay = false,
+	comparePlay = false;
 
+// Comparisions
+function fsCompare() {
+	comparePlay = true;
+	$(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+		default_offset_pct: 0.5,
+		before_label: 'Camera thường', // Set a custom before label
+		after_label: 'Camera Live 4', // Set a custom after label
 	});
 
-	// Open select
-	$('.fs-select-header').on('click', function (e) {
-		var box = $(this).parent();
-		box.parent().removeClass('fs-show-error');
-		if (box.hasClass('fs-open-select')) {
-			box.removeClass('fs-open-select');
-		} else {
-			$('.fs-select').removeClass('fs-open-select');
-			box.addClass('fs-open-select');
-		}
-	});
-
-	// Chose selected item
-	$(document).on('click', '.fs-select-box li', function (e) {
-		var that = $(this);
-		var box = $(this).parent().parent().parent();
-		if (!that.hasClass('selected')) {
-			box.find('li').removeClass('selected');
-			that.addClass('selected');
-			box.removeClass('fs-open-select');
-			box.find('.fs-select-header span').html(that.text());
-		}
-	});
-
-	// Open popup
-	$('.fs-btn-open-overlay').on('click', function () {
-		openOverlay();
-	});
-
-	// Close popup
-	$('.fs-close-overlay').on('click', function () {
-		closeOverlay();
-	});
-
-	// Go top
-	$('.fs-top').on('click', function () {
-		$("html, body").stop().animate({ scrollTop: 0 }, 500);
-	});
-
-	//Close any Tooltip when click out
-	$(document).on('click touchstart', function (event) {
-		//Close select
-		if ($(".fs-select").has(event.target).length == 0 && !$(".fs-select").is(event.target)) {
-			$(".fs-select").removeClass("fs-open-select");
-		}
-	});
-
-	inputHolder();
-
+	// if ($('.enType').length) {
+	//     $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+	//         default_offset_pct: 0.5,
+	//         before_label: 'Normal Mode', // Set a custom before label
+	//         after_label: 'Bokeh Mode', // Set a custom after label
+	//     });
+	// } else {
+	//     $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+	//         default_offset_pct: 0.5,
+	//         before_label: 'Chế độ thường', // Set a custom before label
+	//         after_label: 'Chế độ xóa phông', // Set a custom after label
+	//     });
+	// }
 
 }
 
 // Create Slider
-var swiperThree = null,
-	swiperFull = null;
-
+var first = true;
 function fsSlider() {
 
-	if ($('.fs-slide-full').length) {
-		swiperFull = new Swiper('.fs-slide-full', {
-			effect: 'fade',
-			loop: true,
-			speed: 800,
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false
-			},
-			a11y: {
-				enabled: false
-			}
-		});
-	}
-
-	if ($('.fs-slide-three').length) {
-		swiperThree = new Swiper('.fs-slide-three', {
+	if ($('.fs-xoa-phong-info').length) {
+		cameraSlider = new Swiper('.xoa-phong-slider', {
 			effect: 'slide',
 			loop: true,
 			speed: 800,
-			preventClicksPropagation: false,
-			slidesPerView: 3,
-			breakpoints: {
-				520: {
-					slidesPerView: 1
-				},
-				840: {
-					slidesPerView: 2
-				}
-			},
 			pagination: {
-				el: '.swiper-pagination',
+				el: '.fs-xoa-phong-info .swiper-pagination',
 				clickable: true,
-			},
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
 			},
 			a11y: {
 				enabled: false
+			},
+			navigation: {
+				nextEl: '.fs-xoa-phong-info .swiper-button-next',
+				prevEl: '.fs-xoa-phong-info .swiper-button-prev',
+			},
+			on: {
+				init: function () {
+				}, transitionStart: function () {
+					if (!first) {
+						var go = $('.swiper-slide-active').attr('data-go');
+						$('.fs-xoa-phong .xoa-phone-item').removeClass('active');
+						$('.fs-xoa-phong .xoa-phone-item[data-target=' + go + ']').addClass('active');
+
+						if (go == 'xoa-phong') {
+							$('#imgUnder').attr('src', 'images/xoa-phong-02.png');
+							$('#imgOver').attr('src', 'images/xoa-phong-01.png');
+						} else if (go == 'macro') {
+							$('#imgUnder').attr('src', 'images/macro-02.png');
+							$('#imgOver').attr('src', 'images/macro-01.png');
+						} else if (go == 'chup-dem') {
+							$('#imgUnder').attr('src', 'images/chup-dem-02.png');
+							$('#imgOver').attr('src', 'images/chup-dem-01.png');
+						}
+					}
+					first = false;
+
+				}, transitionEnd: function () {
+				}
 			}
 		});
+
 	}
 
+
+
 }
 
-function fsSliderUpdate() {
+// Events Common
+function fsEvent() {
 
-	if (swiperThree) {
-		swiperThree.update();
+	//Nav Toogle
+	$('.menu_toggler').click(function () {
+		if ($(this).hasClass('active')) {
+			$('body').removeClass('fs-no-scroll');
+			$(this).removeClass('active');
+			$('.menu_primary').removeClass('is-open');
+		} else {
+			$('body').addClass('fs-no-scroll');
+			$(this).addClass('active');
+			$('.menu_primary').addClass('is-open');
+		}
+	});
+
+	//Open Product Nav
+	$('.product-toggle').click(function () {
+		if ($(this).hasClass('active')) {
+			$('body').removeClass('fs-no-scroll');
+			$(this).removeClass('active');
+			$('.fs-menu-products').removeClass('is-open');
+		} else {
+			$('body').addClass('fs-no-scroll');
+			$(this).addClass('active');
+			$('.fs-menu-products').addClass('is-open');
+		}
+	});
+
+	//Close Product Nav
+	$('.menu_products_close').click(function () {
+		$('body').removeClass('fs-no-scroll');
+		$('.product-toggle').removeClass('active');
+		$('.fs-menu-products').removeClass('is-open');
+	});
+
+	inputHolder();
+
+}
+
+
+var liveFull = null;
+function initFull() {
+	if ($('#fullpage')) {
+		liveFull = new fullpage('#fullpage', {
+			licenseKey: '27C6B8F7-DED74693-98572001-47446410',
+			parallax: true,
+			onLeave: function (origin, destination, direction) {
+				console.log(destination.index);
+
+				if (destination.index == 3 || destination.index == 1) {
+					$('.fs-sac-nhanh').addClass('off');
+				} else {
+					$('.fs-sac-nhanh').removeClass('off');
+				}
+				if (destination.index == 7 || destination.index == 5) {
+					$('.fs-mau-sac').addClass('off');
+				} else {
+					$('.fs-mau-sac').removeClass('off');
+				}
+				if (destination.index == 8 || destination.index == 6) {
+					$('.fs-cam-bien').addClass('off');
+				} else {
+					$('.fs-cam-bien').removeClass('off');
+				}
+
+			},
+			afterLoad: function (origin, destination, direction) {
+
+			}
+		});
+
 	}
-
-	if (swiperFull) {
-		swiperFull.update();
-	}
-
 }
 
-// Popup
-function openOverlay() {
-	$('.fs-overlay').addClass('fs-show-overlay');
-	lockBody();
-}
 
-function closeOverlay() {
-	unlockBody();
-	$('.fs-overlay').removeClass('fs-show-overlay');
-}
-
-// Menu
-function openMenu() { }
-
-function closeMenu() { }
 
 // Variables for Scroll
 var isCroll = false,
-	curPos = 0,
 	scrollPos = 0,
 	threshold = 100;
-
-// LazyLoad
-function ImgLazyLoad() {
-
-	lazyImages = document.querySelectorAll('.cmPic.fs-lazy');
-	lazyBgs = document.querySelectorAll('.cmBg.fs-lazy');
-
-	// Lazy images
-	[].slice.call(lazyImages).forEach(function (elm) {
-		if (elm.getBoundingClientRect().top <= window.innerHeight + threshold) {
-			elm.setAttribute('src', elm.getAttribute('data-src'));
-			elm.classList.remove('fs-lazy');
-		}
-	});
-
-	// Lazy background
-	[].slice.call(lazyBgs).forEach(function (elm) {
-		if (elm.getBoundingClientRect().top <= window.innerHeight + threshold) {
-			elm.style.backgroundImage = 'url(' + elm.getAttribute('data-src') + ')';
-			elm.classList.remove('fs-lazy');
-		}
-	});
-
-}
-
 
 // Func Scroll
 function onScroll() {
 	setTimeout(function () {
+
 		scrollPos = $(window).scrollTop();
 
-		if (isCroll) { // Anythings which you want progress after allow scroll
-			if (scrollPos >= window.innerHeight / 2) {
-				$('.fs-top').addClass('fs-show-top');
-			} else {
-				$('.fs-top').removeClass('fs-show-top');
-			}
+		if (isCroll) {
+
 		}
 
-		ImgLazyLoad();
 	}, 0);  // Process for Input Delay
 
 }
@@ -237,30 +192,10 @@ function Resize() {
 
 	// Need detect not mobile when resize because in mobile scrolling call resize
 	if (!isMobile) {
-		ImgLazyLoad();
-		fsSliderUpdate();
 	}
-
-	// unlock body when rezie desktop
-	if (window.innerWidth > 1100 && $('.fs-nav-but').hasClass('active')) {
-		$('.fs-nav-but, .fs-navigation').removeClass('active');
-		unlockBody();
-	}
-
 }
 
-// Func Rotate
 function Rotate() {
-	if (window.orientation == 0) {
-		if (isOverlay) {
-			docEl.removeClass('fs-no-scroll');
-			setTimeout(function () {
-				docEl.addClass('fs-no-scroll');
-			}, 400);
-		}
-	}
-	ImgLazyLoad();
-
 }
 
 // Set Scroll for Page
@@ -272,34 +207,41 @@ $(window).on('resize', Resize);
 // Page Rotate
 $(window).on('orientationchange', Rotate);
 
+var loading = true;
+
+function starPage() {
+
+	if (loading) {
+		loading = false;
+		$('.fs-loading').fadeOut(150, function () {
+
+			fsSlider();
+			fsCompare();
+			fsEvent();
+
+		});
+
+	}
+
+}
+
 //  Page load
 $(window).on('load', function () {
 
-	setTimeout(function () {
-		onScroll(); // must be call here fisrt
-
-		// Call slider here
-		fsSlider();
-
-		$('html,body').scrollTop(0);
-		// Fade Page [ this can edit for each projects]
-		$('body').css({ 'opacity': 1, 'visibility': 'visible', 'animation': '0s ease 0s 1 normal none running none' });
-		isCroll = true;
-		fsEvent();
-	}, 100); // 100ms is detected good
+	if (loading) {
+		starPage();
+	}
 
 });
 
 // Page Ready
 (function () {
-	// Declare variable
-	docEl = $('html, body');
-	pageWrap = $('.fs-page');
-	ImgLazyLoad(); // must be call here fisrt
 
-	$("#flipbook").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
-	  });
+	setTimeout(function () {
+		if (loading) {
+			starPage();
+		}
+	}, 3000);
+	initFull();
+
 })();
